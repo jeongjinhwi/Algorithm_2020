0d3dfc50f3f8e3d279ee832e547c30b000ffcc90
Jeong Jin Hwi <wlsgnl5069@cnu.ac.kr>
2020-01-05 14:08:05 +0900

    Algorithm_hash_01

--- Programmers_hash/src/num1/Solution.java
+++ Programmers_hash/src/num1/Solution.java
@@ -1,5 +1,28 @@
 package num1;
 
-public class Solution {
+import java.util.HashMap;
 
+public class Solution {
+	public String solution(String[] participant, String[] completion) {
+	       String answer = "";
+	        HashMap<String, Integer> hashMap = new HashMap<>();
+
+	        for (String s : participant) {
+	            if (!hashMap.containsKey(s)) {
+	                hashMap.put(s, 0);
+	            }
+	            hashMap.put(s, hashMap.get(s) + 1);
+	        }
+
+	        for (String s : completion) {
+	            hashMap.put(s, hashMap.get(s) - 1);
+	        }
+
+	        for (String s : hashMap.keySet()) {
+	            if (hashMap.get(s) != 0) {
+	                return s;
+	            }
+	        }
+	        return answer;
+	    }
 }
